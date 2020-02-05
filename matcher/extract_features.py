d23402cac360540dab5c07b2df6d4f04846b5897
gianscarpe <g.scarpellini1@campus.unimib.it>
2020-02-05 17:22:10 +0100

    KD tree for IR

--- matcher/extract_features.py
+++ matcher/extract_features.py
@@ -5,6 +5,7 @@ import torchvision.transforms.functional as TF
 from torch.nn import Sequential
 import numpy as np
 
+
 def get_feature_extractor(model):
     model.pre_net.classifier = Sequential()
 
@@ -26,11 +27,14 @@ if __name__ == '__main__':
 
     images = os.listdir(config['data_path'])
     print('Extracting features')
+    features = []
     for img_id in images:
         img_id = img_id[:-4]
         image_path = os.path.join(config['data_path'], str(img_id) + '.jpg')
         image = Image.open(image_path).convert('RGB').resize(config['image_size'])
         x = TF.to_tensor(image)
         feature = model(x.unsqueeze(0))
-        np.save(os.path.join(config['save_path'], str(img_id) + '.npy'), feature.numpy())
+        features.append(feature.numpy())
+
+    np.save(os.path.join(config['save_path'], 'features.npy'), features)
     print('Done')
--- matcher/more_similar_feature.py
+++ matcher/more_similar_feature.py
@@ -4,7 +4,9 @@ from PIL import Image
 import torchvision.transforms.functional as TF
 from torch.nn import Sequential
 import numpy as np
-
+from sklearn.neighbors import KDTree
+import time
+import pickle
 
 def get_feature_extractor(model):
     model.pre_net.classifier = Sequential()
@@ -17,8 +19,12 @@ if __name__ == '__main__':
         'exp_base_dir': 'data/exps/exp1',
         'image_size': [224, 224],
         'load_path': "data/exps/exp1/classification_001.pt",
-        'features_path': 'data/fashion-product-images-small/features',
+        'features_path': 'data/fashion-product-images-small/features/features.npy',
+        'index_path': 'data/fashion-product-images-small/features/index.pickle',
     }
+
+    with open(config['index_path'], "rb") as pic:
+        index = pickle.load(pic)
 
     print("Loading model")
     model = torch.load(config['load_path'])
@@ -29,23 +35,20 @@ if __name__ == '__main__':
 
     x = TF.to_tensor(image)
     feature = model(x.unsqueeze(0))
-    features_path = os.listdir(config['features_path'])
-    print('Extracting features')
+    print('Loading features')
+    t0 = time.time()
 
-    min_sim = 999
-    max_id = 0
-    for feature_id in features_path:
-        feature_path = os.path.join(config['features_path'], feature_id)
-        ft = np.load(feature_path)
+    features_matrix = np.squeeze(np.load(config['features_path']))
+    tree = KDTree(features_matrix)
+    print("Loaded in {}s".format(time.time() - t0))
 
-        sim = np.linalg.norm(feature - ft)
-        if sim < min_sim:
-            min_sim = sim
-            max_id = feature_id
+    t0 = time.time()
+    print("Looking for ...")
+    similar = tree.query(feature, return_distance=False)
 
-    print('More similar feature is {}'.format(max_id))
+    print("Found in {}s".format(t0 - time.time()))
 
-    img_id = max_id[:-4]
+    img_id = index[similar[0][0]][:-4]
     image_path = os.path.join(config['data_path'], str(img_id) + '.jpg')
     image = Image.open(image_path).convert('RGB').resize(config['image_size'])
     image.show()
